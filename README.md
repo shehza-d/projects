5a886db5c96e32f8984180add57577c2eb77ef8e
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-06-06 02:08:55 +0500

    some projects added

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
-# projects
-All my projects
-
+# All my projects
 
 ```json
 [
   {
     "id": "1",
-    "title": "Panaverse Website",
-    "previewImg": "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
-    "description": "A site for Panaverse Education",
+    "title": "Governors Inicaive Website ",
+    "previewImg": "",
+    "description": "An official website for Governor Sindh Inicative for AI, Web3 and Metaverse",
     "techUsed": ["NextJS 13", "TailwindCSS", "TypeScript"],
-    "repoLink": "https://github.com/shehza-d/panaverse-site/",
-    "hostedLink": "https://panavercity-by-shehzad.vercel.app/"
+    "features": ["Dynamic routing", "Form Submission"],
+    "repoLink": "https://github.com/governor-sindh/website",
+    "hostedLink": "https://www.governorsindh.com/"
   },
   {
     "id": "2",
@@ -19,36 +18,96 @@ All my projects
     "previewImg": "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250971/portfolio-ss/e32i6sdh1mzc72pdawm6.png",
     "description": "Dynamic News App with Bing API",
     "techUsed": ["ReactJS", "Dynamic", "API"],
-    "repoLink": "https://github.com/shehza-d/News-App/",
-    "hostedLink": "https://shehzad-news-app.web.app/"
-  },
-  {
-    "id": "1",
-    "title": "Panaverse Website",
-    "previewImg": "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
-    "description": "A site for Panaverse Education",
-    "techUsed": ["NextJS 13", "TailwindCSS", "TypeScript"],
-    "repoLink": "https://github.com/shehza-d/panaverse-site/",
-    "hostedLink": "https://panavercity-by-shehzad.vercel.app/"
-  },
-  {
-    "id": "2",
-    "title": "News App",
-    "previewImg": "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250971/portfolio-ss/e32i6sdh1mzc72pdawm6.png",
-    "description": "Dynamic News App with Bing API",
-    "techUsed": ["ReactJS", "Dynamic", "API"],
+    "features": [""],
     "repoLink": "https://github.com/shehza-d/News-App/",
     "hostedLink": "https://shehzad-news-app.web.app/"
   },
   {
     "id": "3",
+    "title": "Panaverse Website",
+    "previewImg": "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
+    "description": "A site for Panaverse Education",
+    "techUsed": ["NextJS 13", "TailwindCSS", "TypeScript"],
+    "features": ["Dark Mode"],
+    "repoLink": "https://github.com/shehza-d/panaverse-site/",
+    "hostedLink": "https://panavercity-by-shehzad.vercel.app/"
+  },
+  {
+    "id": "4",
+    "title": "Twitter Clone",
+    "previewImg": "",
+    "description": "",
+    "techUsed": ["ReactJS", "Firebase", "Cloudinary", "netlify"],
+    "features": [""],
+    "repoLink": "https://github.com/shehza-d/twitter-clone",
+    "hostedLink": "https://shehzad-twitter.netlify.app/"
+  },
+  {
+    "id": "5",
     "title": "CLI Calculator",
     "previewImg": "",
     "description": "A CLI Calculator based on NodeJS with TypeScript",
     "techUsed": ["NODE JS", "TypeScript"],
+    "features": [""],
     "repoLink": "https://github.com/shehza-d/",
-    "hostedLink": "npm link"
+    "hostedLink": "npx shehzad-cal"
+  },
+  {
+    "id": "6",
+    "title": "Personal Portfolio",
+    "previewImg": "",
+    "description": "",
+    "techUsed": ["NextJS", "TailwindCSS", "TypeScript", "animation CSS"],
+    "features": [
+      "Custom cursor",
+      "Drawing name svg on scroll",
+      "Only 1 external library used",
+      "First app that was designed by me"
+    ],
+    "repoLink": "https://github.com/shehza-d/portfolio",
+    "hostedLink": "https://shehzad.vercel.app"
+  },
+  {
+    "id": "6",
+    "title": "e commerce store",
+    "previewImg": "",
+    "description": "Payment integration with Stripe and",
+    "techUsed": [
+      "NextJS",
+      "TypeScript",
+      "Stripe",
+      "Sanity CMS",
+      "Context API",
+      "Drizzle ORM",
+      "Serverless APIs"
+    ],
+    "features": [
+      "Dynamic Routing",
+      "Payment integration",
+      "Cart functionality"
+    ],
+    "repoLink": "https://github.com/shehza-d/e-commerce",
+    "hostedLink": ""
+  },
+  {
+    "id": "6",
+    "title": "Forms",
+    "previewImg": "",
+    "description": "",
+    "techUsed": ["ReactJS", "Formik", "Yup", "ExpressJS"],
+    "features": [""],
+    "repoLink": "https://github.com/shehza-d/forms",
+    "hostedLink": ""
+  },
+  {
+    "id": "6",
+    "title": "",
+    "previewImg": "",
+    "description": "",
+    "techUsed": [""],
+    "features": [""],
+    "repoLink": "https://github.com/shehza-d/",
+    "hostedLink": ""
   }
 ]
-
 ```
--- projects.ts
+++ projects.ts
@@ -4,20 +4,22 @@ interface IProject {
   previewImg: string;
   description: string;
   techUsed: string[];
+  features: string[];
   repoLink: string;
   hostedLink: string;
 }
 
-export const topProjects: IProject[] = [
+export const projects: IProject[] = [
   {
     id: "1",
-    title: "Panaverse Website",
-    previewImg:
-      "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
-    description: "A site for Panaverse Education",
+    title: "Governors Inicaive Website ",
+    previewImg: "",
+    description:
+      "An official website for Governor Sindh Inicative for AI, Web3 and Metaverse",
     techUsed: ["NextJS 13", "TailwindCSS", "TypeScript"],
-    repoLink: "https://github.com/shehza-d/panaverse-site/",
-    hostedLink: "https://panavercity-by-shehzad.vercel.app/",
+    features: ["Dynamic routing", "Form Submission"],
+    repoLink: "https://github.com/governor-sindh/website",
+    hostedLink: "https://www.governorsindh.com/",
   },
   {
     id: "2",
@@ -26,48 +28,92 @@ export const topProjects: IProject[] = [
       "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250971/portfolio-ss/e32i6sdh1mzc72pdawm6.png",
     description: "Dynamic News App with Bing API",
     techUsed: ["ReactJS", "Dynamic", "API"],
-    repoLink: "https://github.com/shehza-d/News-App/",
-    hostedLink: "https://shehzad-news-app.web.app/",
-  },
-];
-
-export const allProjects: IProject[] = [
-  {
-    id: "1",
-    title: "Panaverse Website",
-    previewImg:
-      "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
-    description: "A site for Panaverse Education",
-    techUsed: ["NextJS 13", "TailwindCSS", "TypeScript"],
-    repoLink: "https://github.com/shehza-d/panaverse-site/",
-    hostedLink: "https://panavercity-by-shehzad.vercel.app/",
-  },
-  {
-    id: "2",
-    title: "News App",
-    previewImg:
-      "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250971/portfolio-ss/e32i6sdh1mzc72pdawm6.png",
-    description: "Dynamic News App with Bing API",
-    techUsed: ["ReactJS", "Dynamic", "API"],
+    features: [""],
     repoLink: "https://github.com/shehza-d/News-App/",
     hostedLink: "https://shehzad-news-app.web.app/",
   },
   {
     id: "3",
+    title: "Panaverse Website",
+    previewImg:
+      "https://res.cloudinary.com/deh1sqok6/image/upload/v1679250981/portfolio-ss/tpjt0re3wsw0wajsoemq.png",
+    description: "A site for Panaverse Education",
+    techUsed: ["NextJS 13", "TailwindCSS", "TypeScript"],
+    features: ["Dark Mode"],
+    repoLink: "https://github.com/shehza-d/panaverse-site/",
+    hostedLink: "https://panavercity-by-shehzad.vercel.app/",
+  },
+  {
+    id: "4",
+    title: "Twitter Clone",
+    previewImg: "",
+    description: "",
+    techUsed: ["ReactJS", "Firebase", "Cloudinary", "netlify"],
+    features: [""],
+    repoLink: "https://github.com/shehza-d/twitter-clone",
+    hostedLink: "https://shehzad-twitter.netlify.app/",
+  },
+  {
+    id: "5",
     title: "CLI Calculator",
     previewImg: "",
     description: "A CLI Calculator based on NodeJS with TypeScript",
     techUsed: ["NODE JS", "TypeScript"],
+    features: [""],
     repoLink: "https://github.com/shehza-d/",
-    hostedLink: "npm link",
+    hostedLink: "npx shehzad-cal",
   },
-  // {
-  //   id: "4",
-  //   title: "Governor",
-  //   previewImg: "",
-  //   description: "",
-  //   techUsed: [""],
-  //   repoLink: "https://github.com/shehza-d/",
-  //   hostedLink: "",
-  // },
+  {
+    id: "6",
+    title: "Personal Portfolio",
+    previewImg: "",
+    description: "",
+    techUsed: ["NextJS", "TailwindCSS", "TypeScript", "animation CSS"],
+    features: [
+      "Custom cursor",
+      "Drawing name svg on scroll",
+      "Only 1 external library used",
+      "First app that was designed by me",
+    ],
+    repoLink: "https://github.com/shehza-d/portfolio",
+    hostedLink: "https://shehzad.vercel.app",
+  },
+  {
+    id: "6",
+    title: "e commerce store",
+    previewImg: "",
+    description: "Payment integration with Stripe and",
+    techUsed: [
+      "NextJS",
+      "TypeScript",
+      "Stripe",
+      "Sanity CMS",
+      "Context API",
+      "Drizzle ORM",
+      "Serverless APIs",
+    ],
+    features: ["Dynamic Routing", "Payment integration", "Cart functionality"],
+    repoLink: "https://github.com/shehza-d/e-commerce",
+    hostedLink: "",
+  },
+  {
+    id: "6",
+    title: "Forms",
+    previewImg: "",
+    description: "",
+    techUsed: ["ReactJS", "Formik", "Yup", "ExpressJS"],
+    features: [""],
+    repoLink: "https://github.com/shehza-d/forms",
+    hostedLink: "",
+  },
+  {
+    id: "6",
+    title: "",
+    previewImg: "",
+    description: "",
+    techUsed: [""],
+    features: [""],
+    repoLink: "https://github.com/shehza-d/",
+    hostedLink: "",
+  },
 ];
